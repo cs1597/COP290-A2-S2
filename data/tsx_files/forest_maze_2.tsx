b231591f703a1a93e12a49e85f851fc3275ea0bd
Vishakha Agarwal <cs1221097@iitd.ac.in>
2024-04-18 20:07:38 +0530

    added puzzle

---
+++ data/tsx_files/forest_maze_2.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="forest_maze_2" tilewidth="753" tileheight="755" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="753" height="755" source="../../graphics/levels/forest_maze_2/maze.png"/>
+ </tile>
+</tileset>
